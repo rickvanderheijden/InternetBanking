bd8c7bee881bef18f4b11d957c5024398460f818
Rick <r.vanderheyden@student.fontys.nl>
2018-05-12 17:58:13 +0200

    Fix BankingApplication

--- BankingApplication/src/main/java/com/ark/bankingapplication/views/StartUp.java
+++ BankingApplication/src/main/java/com/ark/bankingapplication/views/StartUp.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package main.java.com.ark.bankingapplication.views;
+package com.ark.bankingapplication.views;
 
 import java.io.IOException;
 import java.net.URL;
