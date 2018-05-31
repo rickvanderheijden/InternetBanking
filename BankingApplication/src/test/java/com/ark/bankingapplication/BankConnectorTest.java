0831d0cb5e2389f0f9a659efa7383b27df9761cd
ArthurDoorgeest <31449856+arthurdoorgeest@users.noreply.github.com>
2018-05-31 23:00:46 +0200

    Added a test, not sure if it's correct.

---
+++ BankingApplication/src/test/java/com/ark/bankingapplication/BankConnectorTest.java
@@ -0,0 +1,60 @@
+package com.ark.bankingapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BankConnectorTest {
+
+    @Test
+    public void getSessionKey() {
+    }
+
+    @Test
+    public void connect() {
+    }
+
+    @Test
+    public void setSessionKey() {
+    }
+
+    @Test
+    public void getIBankForClientSession() {
+    }
+
+    @Test
+    public void getIBankForClientLogin() {
+    }
+
+    @Test
+    public void login() {
+    }
+
+    @Test
+    public void createCustomer() {
+    }
+
+    @Test
+    public void createBankAccount() {
+    }
+
+    @Test
+    public void getBankAccountNumbers() {
+    }
+
+    @Test
+    public void executeTransaction() {
+    }
+
+    @Test
+    public void getCustomer() {
+    }
+
+    @Test
+    public void getBankAccount() {
+    }
+
+    @Test
+    public void propertyChange() {
+    }
+}
