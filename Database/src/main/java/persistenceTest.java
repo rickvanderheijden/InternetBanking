84a2b1bb33cd16c836c95e00715e0b565e693580
ArthurDoorgeest <31449856+ArthurDoorgeest@users.noreply.github.com>
2018-06-10 14:14:41 +0200

    Added DAtabase to maven and project structure, did some refactor to names

--- Database/src/main/java/persistenceTest.java
+++ Database/src/main/java/persistenceTest.java
@@ -6,9 +6,7 @@ import org.junit.Test;
 
 import java.util.List;
 
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertNull;
-import static junit.framework.TestCase.assertTrue;
+import static junit.framework.TestCase.*;
 
 
 /**
